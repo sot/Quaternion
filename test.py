d628402206aff767ae1ab5d4408cf5073e3ac355
Tom Aldcroft <taldcroft@gmail.com>
2016-07-12 16:23:17 -0400

    PEP8 fixes

--- test.py
+++ test.py
@@ -1,41 +1,50 @@
 import numpy as np
 from Quaternion import Quat
-from nose.tools import *
 
 ra = 10.
 dec = 20.
 roll = 30.
-q0 = Quat([ra,dec,roll])
+q0 = Quat([ra, dec, roll])
+
 
 def test_from_eq():
     q = Quat([ra, dec, roll])
     print 'q.equatorial=', q.equatorial
-    assert_almost_equal(q.q[0], 0.26853582)
-    assert_almost_equal(q.q[1], -0.14487813)
-    assert_almost_equal(q.q[2],  0.12767944)
-    assert_almost_equal(q.q[3],  0.94371436)
+    assert np.allclose(q.q[0], 0.26853582)
+    assert np.allclose(q.q[1], -0.14487813)
+    assert np.allclose(q.q[2],  0.12767944)
+    assert np.allclose(q.q[3],  0.94371436)
+
 
 def test_from_transform():
     """Initialize from inverse of q0 via transform matrix"""
     q = Quat(q0.transform.transpose())
-    assert_almost_equal(q.q[0], -0.26853582)
-    assert_almost_equal(q.q[1], 0.14487813)
-    assert_almost_equal(q.q[2], -0.12767944)
-    assert_almost_equal(q.q[3],  0.94371436)
+    assert np.allclose(q.q[0], -0.26853582)
+    assert np.allclose(q.q[1], 0.14487813)
+    assert np.allclose(q.q[2], -0.12767944)
+    assert np.allclose(q.q[3],  0.94371436)
+
 
 def test_inv_eq():
     q = Quat(q0.equatorial)
     t = q.transform
     tinv = q.inv().transform
     t_tinv = np.dot(t, tinv)
-    for v1, v2 in zip(t_tinv.flatten(), [1,0,0,0,1,0,0,0,1]):
-        assert_almost_equal(v1, v2)
+    for v1, v2 in zip(t_tinv.flatten(), [1, 0, 0, 0, 1, 0, 0, 0, 1]):
+        assert np.allclose(v1, v2)
+
 
 def test_inv_q():
     q = Quat(q0.q)
     t = q.transform
     tinv = q.inv().transform
     t_tinv = np.dot(t, tinv)
-    for v1, v2 in zip(t_tinv.flatten(), [1,0,0,0,1,0,0,0,1]):
-        assert_almost_equal(v1, v2)
+    for v1, v2 in zip(t_tinv.flatten(), [1, 0, 0, 0, 1, 0, 0, 0, 1]):
+        assert np.allclose(v1, v2)
 
+
+def test_dq():
+    q1 = Quat((20, 30, 0))
+    q2 = Quat((20, 30.1, 1))
+    dq = q1.dq(q2)
+    assert np.allclose(dq.equatorial, (0, 0.1, 1))
