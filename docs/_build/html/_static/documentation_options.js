118980fd649f221c6064b3ee2c1803061968c0df
Javier Gonzalez <javier.gonzalez@cfa.harvard.edu>
2020-09-15 14:04:42 -0400

    check in documentation

---
+++ docs/_build/html/_static/documentation_options.js
@@ -0,0 +1,12 @@
+var DOCUMENTATION_OPTIONS = {
+    URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
+    VERSION: '3.5.3.dev5+g3f9b708',
+    LANGUAGE: 'None',
+    COLLAPSE_INDEX: false,
+    BUILDER: 'html',
+    FILE_SUFFIX: '.html',
+    LINK_SUFFIX: '.html',
+    HAS_SOURCE: true,
+    SOURCELINK_SUFFIX: '.txt',
+    NAVIGATION_WITH_KEYS: false
+};
